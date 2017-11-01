0d1d882c7a1a3b1d32b63f6380e0054b77fd7f34
rrurr <shreyasamargol@my.unt.edu>
2017-10-31 20:52:53 -0500

    Init

---
+++ usaco-master/template.C
@@ -0,0 +1,19 @@
+/*
+ID: nissan.po1
+LANG: C++
+TASK: 
+*/
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+}
